4316860022a038a83affc246a5dd17a9cdec8636
Jörg Vater <joerg.vater@citrix.com>
2015-10-28 10:47:28 +0100

    add teams controller in admin

---
+++ administrator/controllers/teams.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Access
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+class TTClubTeamsController extends JControllerBase
+{
+
+    /**
+     * Execute the controller.
+     *
+     * @return  boolean  True if controller finished execution, false if the controller did not
+     *                   finish execution. A controller might return false if some precondition for
+     *                   the controller to run has not been satisfied.
+     *
+     * @since   12.1
+     * @throws  LogicException
+     * @throws  RuntimeException
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
---
+++ administrator/ttclub.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joergv
+ * Date: 18.09.2015
+ * Time: 10:22
+ */
+class ttclub
+{
+
+}
