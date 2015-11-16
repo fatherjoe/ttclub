1fe22028d86e1e3d49982b857bae4ba9c93b7bf4
Jörg Vater <joerg.vater@citrix.com>
2015-11-16 20:14:19 +0100

    update copyright information

--- ttc_t3/templateInfo.php
+++ ttc_t3/templateInfo.php
@@ -1,14 +1,10 @@
 <?php
 /** 
  *------------------------------------------------------------------------------
- * @package       T3 Framework for Joomla!
+ * @package       TTC T3 template
  *------------------------------------------------------------------------------
- * @copyright     Copyright (C) 2004-2013 JoomlArt.com. All Rights Reserved.
+ * @copyright     Copyright (C) 2015 TTC Wöschbach 58 e.V.
  * @license       GNU General Public License version 2 or later; see LICENSE.txt
- * @authors       JoomlArt, JoomlaBamboo, (contribute to this project at github 
- *                & Google group to become co-author)
- * @Google group: https://groups.google.com/forum/#!forum/t3fw
- * @Link:         http://t3-framework.org 
  *------------------------------------------------------------------------------
  */
 
